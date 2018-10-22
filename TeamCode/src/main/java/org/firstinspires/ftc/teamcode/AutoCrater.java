4cb5d2c61719aceb3b61577731b032ebb3e63c28
Garrett Stevenson <garrettmstevenson@gmail.com>
2018-10-22 17:24:42 -0500

    Merge branch 'master' of C:\Users\vhsrobotics\Documents\GitHub\6210_Software_2018-2019 with conflicts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCrater.java
@@ -14,39 +14,47 @@ public class AutoCrater extends AutoLinearOpMode {
 
         //Use gamepad buttons to determine wait time
 
-
-      //  telemetry.addData("Wait time: ", "WAIT TIME HERE");
+        telemetry.addData("Wait time: ", "WAIT TIME HERE");
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        boolean hit = false;
-
         waitForStart();
-           //move forward
-            driveDistance(0.3,5);
-            //Turn 90
-            rotate(75,0.3);
-            //Back up
-            driveDistance(0.3, -4);
-            if(isGold() && !hit)
-                knockGold();
-                hit = true;
-            driveDistance(0.3, 4);
-            if(isGold() && !hit)
-                knockGold();
-                hit = true;
-            driveDistance(0.3, 4);
-                knockGold();
-                hit = true;
-
-            telemetry.addData("status ", "done");
-        //turn towards wall
-            /*rotate(55, 0.3);
-             //move towards wall
-            driveDistance(0.3, 7);
-            rotate(50, 0.1);
+            // If the cube is on the left side
+            //drive up to cube
+            driveDistance(0.3, 10);
+            sleep(1000);
+            //turn parallel to mineral line
+            rotate(-80, 0.2);
+            sleep(1000);
+            //drive to farthest cube from wall connecting team depot to crater
+            driveDistance(-0.3, 7);
+            sleep(1000);
+            //Start color sensor scanning
+            //Move forward until next to wall
+            driveDistance(0.3, 17);
             //turn towards depot
-            rotate(37, 0.3);*/
+            rotate(30, 0.2 );
+
+
+            /*
+            //move towards wall
+            driveDistance(0.3, 25);
+            sleep(1000);
+            //turn towards depot
+            rotate(37, 0.3);
+*/
+
+            //driveDistance(0.3, -5);
+            //driveDistance(0.3,10);
+
+            //lower robot
+            //turn left to right while scanning for gold (tbd)
+            //when find gold stop and get angle to gold
+            //move forward and knock off gold
+            //back up and turn left
+            //move forward until lines up with wall
+            //Move forward to crater and park
+
         }
     }
 
